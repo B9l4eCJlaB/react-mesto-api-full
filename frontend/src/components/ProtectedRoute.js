e2547e724e859f613f2188eeb0e5c8b3c2226a35
Vyacheslav <90191102+B9l4eCJlaB@users.noreply.github.com>
2022-09-20 01:15:40 +0300

    Delete frontend directory

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Route, Redirect } from "react-router-dom";
-
-const ProtectedRoute = ({ component: Component, ...props }) => {
-  return <Route>{() => (props.loggedIn ? <Component {...props} /> : <Redirect to="/signin" />)}</Route>;
-};
-
-export default ProtectedRoute;
